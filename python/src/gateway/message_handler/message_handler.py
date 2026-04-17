11249dd5f9a96e6c8efc605857e73e579197ed5c
valenpontaut <valenpontaut@gmail.com>
2026-04-17 18:10:01 -0300

    feat: propagate client_id through pipeline for multi-client support

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -1,18 +1,21 @@
+import uuid
 from common import message_protocol
 
 
 class MessageHandler:
 
     def __init__(self):
-        pass
-    
+        self.client_id = str(uuid.uuid4())
+
     def serialize_data_message(self, message):
         [fruit, amount] = message
-        return message_protocol.internal.serialize([fruit, amount])
+        return message_protocol.internal.serialize([self.client_id, fruit, amount])
 
     def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([])
+        return message_protocol.internal.serialize([self.client_id])
 
     def deserialize_result_message(self, message):
         fields = message_protocol.internal.deserialize(message)
-        return fields
+        if fields[0] != self.client_id:
+            return None
+        return fields[1]
