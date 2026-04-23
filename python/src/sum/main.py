09ddd054d6392565878af894f3df12657da3d8d0
valenpontaut <valenpontaut@gmail.com>
2026-04-23 00:21:01 -0300

    refactor(sum): replace control exchange with dedicated queues per worker

--- python/src/sum/main.py
+++ python/src/sum/main.py
@@ -11,7 +11,6 @@ MOM_HOST = os.environ["MOM_HOST"]
 INPUT_QUEUE = os.environ["INPUT_QUEUE"]
 SUM_AMOUNT = int(os.environ["SUM_AMOUNT"])
 SUM_PREFIX = os.environ["SUM_PREFIX"]
-SUM_CONTROL_EXCHANGE = "SUM_CONTROL_EXCHANGE"
 AGGREGATION_AMOUNT = int(os.environ["AGGREGATION_AMOUNT"])
 AGGREGATION_PREFIX = os.environ["AGGREGATION_PREFIX"]
 
@@ -21,94 +20,83 @@ class SumFilter:
         self.input_queue = middleware.MessageMiddlewareQueueRabbitMQ(
             MOM_HOST, INPUT_QUEUE
         )
-        self.control_publisher = middleware.MessageMiddlewareExchangeRabbitMQ(
-            MOM_HOST,
-            SUM_CONTROL_EXCHANGE,
-            [f"{SUM_PREFIX}_{i}" for i in range(SUM_AMOUNT)],
+        self.dedicated_queue = middleware.MessageMiddlewareQueueRabbitMQ(
+            MOM_HOST, f"{SUM_PREFIX}_{ID}"
         )
         self.amount_by_fruit = {}
-        self.lock = threading.Lock()
-        self.control_consumer = None
-        self.aggregation_exchange = None
-        self.control_thread = None
+        self.eof_received = {}
+        self.dedicated_consumer = None
+        self.dedicated_thread = None
 
     def _aggregator_idx(self, client_id, fruit):
         key = f"{fruit}_{client_id}".encode()
         return int(hashlib.md5(key).hexdigest(), 16) % AGGREGATION_AMOUNT
 
-    def _process_data(self, client_id, fruit, amount):
-        logging.info(
-            f"Processing data: client={client_id} fruit={fruit} amount={amount}"
+    def _flush(self, client_id):
+        client_data = self.amount_by_fruit.pop(client_id, {})
+        for fi in client_data.values():
+            idx = self._aggregator_idx(client_id, fi.fruit)
+            self.aggregation_exchange.send(
+                message_protocol.internal.serialize([client_id, fi.fruit, fi.amount]),
+                routing_key=f"{AGGREGATION_PREFIX}_{idx}",
+            )
+        client_data.clear()
+        del client_data
+        self.aggregation_exchange.send(
+            message_protocol.internal.serialize([client_id])
         )
-        with self.lock:
+
+    def _forward_to_dedicated(self, message, ack, nack):
+        self.dedicated_queue.send(message)
+        ack()
+
+    def _process_dedicated(self, message, ack, nack):
+        fields = message_protocol.internal.deserialize(message)
+        if len(fields) == 3:
+            client_id, fruit, amount = fields
             client_data = self.amount_by_fruit.setdefault(client_id, {})
             client_data[fruit] = client_data.get(
                 fruit, fruit_item.FruitItem(fruit, 0)
             ) + fruit_item.FruitItem(fruit, int(amount))
-
-    def process_data_message(self, message, ack, nack):
-        fields = message_protocol.internal.deserialize(message)
-        if len(fields) == 3:
-            client_id, fruit, amount = fields
-            self._process_data(client_id, fruit, amount)
         else:
             client_id = fields[0]
-            logging.info(f"EOF client {client_id}, broadcasting to control exchange")
-            self.control_publisher.send(
-                message_protocol.internal.serialize([client_id])
-            )
+            if client_id not in self.eof_received:
+                logging.info(f"First EOF for client {client_id}, propagating and flushing")
+                self.eof_received[client_id] = True
+                for i in range(SUM_AMOUNT):
+                  if i != ID:
+                      q = middleware.MessageMiddlewareQueueRabbitMQ(MOM_HOST, f"{SUM_PREFIX}_{i}")
+                      q.send(message_protocol.internal.serialize([client_id]))
+                      q.close()
+                self._flush(client_id)
+            else:
+                logging.info(f"Ignoring duplicate EOF for client {client_id}")
         ack()
 
-    def _run_control_consumer(self):
-        self.control_consumer = middleware.MessageMiddlewareExchangeRabbitMQ(
-            MOM_HOST, SUM_CONTROL_EXCHANGE, [f"{SUM_PREFIX}_{ID}"]
+    def _run_dedicated_consumer(self):
+        self.dedicated_consumer = middleware.MessageMiddlewareQueueRabbitMQ(
+            MOM_HOST, f"{SUM_PREFIX}_{ID}"
         )
         self.aggregation_exchange = middleware.MessageMiddlewareExchangeRabbitMQ(
             MOM_HOST,
             AGGREGATION_PREFIX,
             [f"{AGGREGATION_PREFIX}_{i}" for i in range(AGGREGATION_AMOUNT)],
         )
-
-        def on_eof(message, ack, nack):
-            client_id = message_protocol.internal.deserialize(message)[0]
-            logging.info(
-                f"EOF received for client {client_id}, flushing accumulated data"
-            )
-            with self.lock:
-                client_data = self.amount_by_fruit.pop(client_id, {})
-            logging.info(
-                f"Sending {len(client_data)} fruit totals to aggregators for client {client_id}"
-            )
-            for fi in client_data.values():
-                idx = self._aggregator_idx(client_id, fi.fruit)
-                self.aggregation_exchange.send(
-                    message_protocol.internal.serialize(
-                        [client_id, fi.fruit, fi.amount]
-                    ),
-                    routing_key=f"{AGGREGATION_PREFIX}_{idx}",
-                )
-            client_data.clear()
-            del client_data
-            logging.info(f"Sending EOF to aggregators for client {client_id}")
-            self.aggregation_exchange.send(
-                message_protocol.internal.serialize([client_id])
-            )
-            ack()
-
-        self.control_consumer.start_consuming(on_eof)
+        self.dedicated_consumer.start_consuming(self._process_dedicated)
         self.aggregation_exchange.close()
 
     def start(self):
-        self.control_thread = threading.Thread(
-            target=self._run_control_consumer, daemon=True
+        self.dedicated_thread = threading.Thread(
+            target=self._run_dedicated_consumer, daemon=True
         )
-        self.control_thread.start()
+        self.dedicated_thread.start()
 
-        self.input_queue.start_consuming(self.process_data_message)
-        if self.control_consumer is not None:
-            self.control_consumer.stop_consuming()
-        self.control_thread.join()
-        self.control_publisher.close()
+        self.input_queue.start_consuming(self._forward_to_dedicated)
+
+        if self.dedicated_consumer is not None:
+            self.dedicated_consumer.stop_consuming()
+        self.dedicated_thread.join()
+        self.dedicated_queue.close()
 
     def stop(self):
         self.input_queue.stop_consuming()
